b3db0813ede9086fc00f512a9ff5bc53f3a20350
Joe Tillotson <joe@sonoftillot.net>
2019-07-17 16:14:32 -0700

    fixes unit tests based on refactoring

--- src/app/callback/callback.component.spec.ts
+++ src/app/callback/callback.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { CallbackComponent } from './callback.component';
+import { AuthService } from '../services/auth.service';
+import { AuthServiceMock } from '../services/auth.service.mock';
 
 describe('CallbackComponent', () => {
   let component: CallbackComponent;
@@ -8,18 +11,18 @@ describe('CallbackComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CallbackComponent ]
+      imports: [ RouterTestingModule ],
+      declarations: [ CallbackComponent ],
+      providers: [
+        { provide: AuthService, useValue: new AuthServiceMock() }
+      ]
     })
     .compileComponents();
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CallbackComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
+  it('should create the component', () => {
+    const fixture = TestBed.createComponent(CallbackComponent);
+    const component = fixture.componentInstance;
     expect(component).toBeTruthy();
   });
 });
